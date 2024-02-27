7b3a69f15eced749dbdcc8add76eda82f28eae12
Aorui Tan <aorui.tan@Aoruis-MacBook-Pro.local>
2024-02-27 17:20:36 +1100

    initial setup of todo list

---
+++ Calculator/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number:");
+int firstNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input the second number:");
+int secondNum = int.Parse(Console.ReadLine());
+
+// What do you want to do with those numbers?
+// [A]dd
+// [S]ubtract
+// [M]ultiply
+
+Console.WriteLine("What do you want to do with those numbers?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[M]ultiply");
+
+string userOption = Console.ReadLine().ToLower();
+
+// if (userOption == "a")
+// {
+//     Console.WriteLine($"{firstNum}+{secondNum}={firstNum+secondNum}");
+// }
+// else if (userOption == "s")
+// {
+//     Console.WriteLine($"{firstNum}-{secondNum}={firstNum-secondNum}");
+// }
+// else if (userOption == "m")
+// {
+//     Console.WriteLine($"{firstNum}*{secondNum}={firstNum*secondNum}");
+// }
+// else
+// {
+//     Console.WriteLine("Invalid option");
+// }
+
+// switch (userOption)
+// {
+//     case "a":
+//     Console.WriteLine($"{firstNum}+{secondNum}={firstNum+secondNum}");
+//     break;
+//     case "s":
+//     Console.WriteLine($"{firstNum}-{secondNum}={firstNum-secondNum}");
+//     break;
+//     case "m":
+//     Console.WriteLine($"{firstNum}*{secondNum}={firstNum*secondNum}");
+//     break;
+//     default:
+//     Console.WriteLine("Invalid option");
+//     break;
+// }
+
+string RepeatCharacter(char character, int targetLength)
+        {
+            //your code goes here
+            int i=0;
+            string result="";
+            do{
+                result = result + character;
+                i++;
+            }
+            while(i<targetLength);
+            
+return result;
+        }
---
+++ todo list/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+var userInput = Console.ReadLine();
+
+Console.WriteLine(userInput);
+
+
+if(userInput == "abc")
+{
+    System.Console.WriteLine(userInput);
+}
+else 
+{
+    System.Console.WriteLine("not the same");
+}
